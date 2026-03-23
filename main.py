aff74b206cfb9b533ad0ddb7133e018f441e3031
Jaeger Nolte <Jaeger.nolte@gmail.com>
2026-03-22 19:13:03 -0500

    collisions

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame;
+import sys;
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state;
+from logger import log_event;
 from player import Player;
+from circleshape import CircleShape;
 from asteroid import Asteroid;
 from asteroidfield import AsteroidField;
 
@@ -18,15 +21,20 @@ def main():
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
 
+
     # player class is added to both groups
     player.add(updatable)
     player.add(drawable)
+
 
     # Player is the name of the class, not an instance of it
     # This must be done before any Player objects are created
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+
+
+    asteroidfield = AsteroidField() # creates new asteroidfield object
 
 
     while True:
@@ -39,9 +47,17 @@ def main():
 
         screen.fill("black")
         updatable.update(dt) # updates objects in the updatable group
+
+        for asteroid in asteroids: # TODO have collider find the POS of self and other
+            if CircleShape.collides_with(player, asteroid) == True:
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
+
         for object in drawable: # loops drawables and .draw() them indiviudally 
             object.draw(screen)
-        pygame.display.flip() # refreshes franes
+
+        pygame.display.flip() # refreshes frames
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
